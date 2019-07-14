dbeeadc3612b80f17453525c30f7de6cacf7233b
Patrick Virtanen <patrick.virtanen@gmail.com>
2019-07-14 21:33:21 +0200

    Updates

---
+++ precache-manifest.796627dfc237f3164f048d4f715c8e3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040e3fc86eaa45fb6f2d489beab71fb2",
+    "url": "/site-try/index.html"
+  },
+  {
+    "revision": "4cbfca00c0a8226cbf00",
+    "url": "/site-try/static/css/main.a805b6de.chunk.css"
+  },
+  {
+    "revision": "29327584ec1725217846",
+    "url": "/site-try/static/js/2.f7261508.chunk.js"
+  },
+  {
+    "revision": "4cbfca00c0a8226cbf00",
+    "url": "/site-try/static/js/main.5f9f1490.chunk.js"
+  },
+  {
+    "revision": "ca231d9f58bc47f4c840",
+    "url": "/site-try/static/js/runtime~main.f8054571.js"
+  }
+]);
